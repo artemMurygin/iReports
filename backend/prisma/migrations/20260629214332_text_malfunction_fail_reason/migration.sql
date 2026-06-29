9d6e84786a92eab59f3c69ceb2ae86fcd0df8f56
artemMurygin <artem@murygin.tech>
2026-06-30 00:43:59 +0300

    One more hotFix

---
+++ backend/prisma/migrations/20260629214332_text_malfunction_fail_reason/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "roapp_orders" ALTER COLUMN "malfunction" SET DATA TYPE TEXT,
+ALTER COLUMN "fail_reason" SET DATA TYPE TEXT;
--- backend/src/utils/initialUploadData.ts
+++ backend/src/utils/initialUploadData.ts
@@ -3,12 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import { BitrixSyncService } from '../sync/bitrix/bitrix.service';
 import { UploadLogger } from './logger';
-import { RoappSyncModule } from '../sync/roapp/roapp.module';
 import { RoappSyncService } from '../sync/roapp/roapp.service';
 import { CustomApiRoappSyncService } from '../sync/custom-api-roapp/custom-api-roapp.service';
 
 async function bootstrap() {
   const dateArg = process.argv[2];
+  const erp = process.argv[3];
+
   if (!dateArg) {
     console.error('Usage: npm run initial -- <date> (e.g. 2025-01-01)');
     process.exit(1);
@@ -27,24 +28,30 @@ async function bootstrap() {
 
   const log = new UploadLogger('Инициализация данных');
   try {
-    // await bitrix.uploadEmployees();
-    // await bitrix.uploadStages();
-    // await bitrix.uploadDeviceTypes();
-    // await bitrix.uploadLeadSources();
-    // await bitrix.uploadEnums();
-    // await bitrix.uploadSources();
-    // await bitrix.uploadCreatedDeals(fromDate);
-    await roapp.uploadEmployees();
-    await roapp.uploadMarketingSources();
-    await roapp.uploadOrderStatuses();
-    await roapp.uploadOrderTypes();
-    await roapp.uploadProductCategories();
-    await roapp.uploadServiceCategories();
-    await roapp.uploadServices();
-    await roapp.uploadProducts();
-    await customRoApp.uploadServicesBonuses();
-    const ordersIds = await roapp.uploadCreatedOrders(fromDate);
-    await roapp.uploadOrderItems(ordersIds);
+    if (erp.includes('B')) {
+      await bitrix.uploadEmployees();
+      await bitrix.uploadStages();
+      await bitrix.uploadDeviceTypes();
+      await bitrix.uploadLeadSources();
+      await bitrix.uploadEnums();
+      await bitrix.uploadSources();
+      await bitrix.uploadCreatedDeals(fromDate);
+    }
+
+    if (erp.includes('R')) {
+      await roapp.uploadEmployees();
+      await roapp.uploadMarketingSources();
+      await roapp.uploadOrderStatuses();
+      await roapp.uploadOrderTypes();
+      await roapp.uploadProductCategories();
+      await roapp.uploadServiceCategories();
+      await roapp.uploadServices();
+      await roapp.uploadProducts();
+      await customRoApp.uploadServicesBonuses();
+      const ordersIds = await roapp.uploadCreatedOrders(fromDate);
+      await roapp.uploadOrderItems(ordersIds);
+    }
+
     await app.close();
   } catch (error) {
     throw new Error();
